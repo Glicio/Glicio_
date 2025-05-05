8fc5ed689def86867d053ef92efca1bd43ca1703
Glicio Pereira Uchoa <glicioo@outlook.com>
2025-05-05 10:11:58 -0300

    removed more text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center text-white">
       <div className="flex flex-col gap-2 rounded-md bg-white/5 p-4 backdrop-blur-sm">
-        <h1 className="text-4xl font-bold">Olá, Mundo!</h1>
-        <p>Eu sou Glicio, programador fullstack e jogador de destiny. </p>
+        <p>Eu sou Glicio</p>
         <p className="w-full min-w-0 max-w-[90vw] md:max-w-[50vw]">
           Atualmente sócio da empresa{" "}
           <a
